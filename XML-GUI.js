462032c2ebc816ca8257c0db7f95baf01fa68392
andrea <andrecasa91@gmail.com>
2020-12-24 15:59:51 +0100

    node server debugged

--- XML-GUI.js
+++ XML-GUI.js
@@ -6,13 +6,15 @@ const fs = require('fs')
 let XMLparser = new addon.xmlJS();
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    console.log("request url: " + req.url);
     if(req.url.localeCompare('/') === 0){
         res.writeHead(200, { 'content-type': 'text/html' });
         fs.createReadStream('XML-GUI.html').pipe(res);
+        console.log("");
         return;
     }
     if(req.url.localeCompare('/favicon.ico') === 0){
+        console.log("");
         return;
     }
 
@@ -22,43 +24,65 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
         }).on('end', () => {
             body = Buffer.concat(body).toString();
+            const resBody = onLoaded(body); 
+            if(resBody === null) {
+                res.writeHead(404);
+                return;
+            }
             res.writeHead(200, { 'content-type': 'text' });
-            res.write(onLoaded(body));
+            res.write(resBody);
             res.end();
         });
     }
 
     // check that is a file to be served
-    let isStaticFile = false;
-    for(let p=0; p<req.url.length; ++p) {
-        if(req.url[p] === '.'){
-            isStaticFile = true;
-            break; 
-        }
+    function getFileExtension(){
+        let dotPosition = null;
+        for(let p=0; p<(req.url.length-1); ++p) {
+            if(req.url[p] === '.') dotPosition = p; 
+        }    
+        if(dotPosition === null) return null;
+        return req.url.slice(dotPosition + 1)
     }
-    if(isStaticFile){
-        res.writeHead(200, { 'content-type': 'text' });
-        fs.createReadStream("./" + req.url).pipe(res);
+    const extension = getFileExtension();
+    if(extension !== null){
+        let contentType = 'text/' + extension; 
+        if(extension === 'svg'){
+            contentType = 'image/svg+xml';
+        }
+        res.writeHead(200, { 'content-type': contentType  });
+        console.log("serving static file of content type" + contentType);
+        console.log("");
+        fs.createReadStream("." + req.url).pipe(res);
         return;
     }
 
     process((request)=>{
+        console.log("respond to GET request");
+        console.log(request);
         vals = JSON.parse(request);
         console.log("req bodies:");
         console.log(vals);
-        console.log("");
+
+        function logAndReturn(result){
+            console.log("result: " + result);
+            console.log("");
+            return result;
+        }
 
         if(vals.length == 0){
-            res = XMLparser.ProcessRequest(req.url);
+            return logAndReturn(XMLparser.ProcessRequest(req.url));
         }
         else if(vals.length == 1){
-            res = XMLparser.ProcessRequest(req.url, vals[0]);
+            return logAndReturn(XMLparser.ProcessRequest(req.url, vals[0]));
         }
         else if(vals.length == 2){
-            res = XMLparser.ProcessRequest(req.url, vals[0], vals[1]);
+            return logAndReturn(XMLparser.ProcessRequest(req.url, vals[0], vals[1]));
         }
         else {
             console.log("too many arguments in request: ");
+            console.log("");
+            return null;
         }
     });
 })
